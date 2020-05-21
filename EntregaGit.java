9704d60b8a508bbc8e9c556f1b08d6cfb3ec608a
Adans Lima <adans.lima@fatec.sp.gov.br>
2020-05-20 23:14:50 -0300

    Agora estamos lendo o nome do usuario

--- EntregaGit.java
+++ EntregaGit.java
@@ -1,5 +1,10 @@
+import javax.javax.swing.*;
+import javax.swing.JOptionPane;
 public class EntregaGit{
+    //TESTEEEEE
     public static void main(String[] args) {
         System.out.println("Entregando com o Git na linha de comando... ");
+        String none = JOptionPane.showOptionDialog('Qual o seu nome?');
+        JOptionPane.showMessageDialog(null, String.format ('Oi, %s', none));
     }
 }
